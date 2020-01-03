ea8ae70256bff81500d188b1171312b21355ddae
Malte Wirz <mail@maltewirz.com>
2020-01-03 17:47:03 +0100

    storing token and auth to fetch working

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import router from './router'
 import store from './store'
 
-// axios.defaults.baseURL = 'https://vue-axios-bbcbe.firebaseio.com'
+axios.defaults.baseURL = 'https://vue-axios-bbcbe.firebaseio.com'
 // axios.defaults.headers.common['Authorization'] ='testTokenxyxyxyx'
 axios.defaults.headers.get['Accepts'] = 'application/json'
 
