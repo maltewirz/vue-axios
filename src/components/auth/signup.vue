533621a5eb141dbbb81d2cea7995f129025979ac
Malte Wirz <mail@maltewirz.com>
2020-01-04 20:47:58 +0100

    vuelidate intro

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -2,32 +2,39 @@
   <div id="signup">
     <div class="signup-form">
       <form @submit.prevent="onSubmit">
-        <div class="input">
+        <div class="input" :class="{invalid: $v.email.$error}">
           <label for="email">Mail</label>
           <input
                   type="email"
                   id="email"
+                  @blur="$v.email.$touch()"
                   v-model="email">
+          <p v-if="!$v.email.email">Please provide a valid email</p>
+          <p v-if="!$v.email.required">This field must not be empty</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.age.$error}">
           <label for="age">Your Age</label>
           <input
                   type="number"
                   id="age"
+                  @blur="$v.age.$touch()"
                   v-model.number="age">
+          <p v-if="!$v.age.minVal">You have to be least {{ $v.age.$params.minVal.min }} years old</p>
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.password.$error}">
           <label for="password">Password</label>
           <input
                   type="password"
                   id="password"
+                  @blur="$v.password.$touch()"
                   v-model="password">
         </div>
-        <div class="input">
+        <div class="input" :class="{invalid: $v.confirmPassword.$error}">
           <label for="confirm-password">Confirm Password</label>
           <input
                   type="password"
                   id="confirm-password"
+                  @blur="$v.confirmPassword.$touch()"
                   v-model="confirmPassword">
         </div>
         <div class="input">
@@ -56,8 +63,12 @@
             </div>
           </div>
         </div>
-        <div class="input inline">
-          <input type="checkbox" id="terms" v-model="terms">
+         <div class="input inline" :class="{invalid: !$v.terms.$model}" >
+          <input
+                  type="checkbox"
+                  id="terms"
+                  @change="$v.terms.$touch()"
+                  v-model="terms">
           <label for="terms">Accept Terms of Use</label>
         </div>
         <div class="submit">
@@ -69,7 +80,8 @@
 </template>
 
 <script>
-
+  import { required, email, numeric, minValue, minLength, sameAs, requiredUnless } from 'vuelidate/lib/validators'
+import { log } from 'util'
   export default {
     data () {
       return {
@@ -80,6 +92,32 @@
         country: 'usa',
         hobbyInputs: [],
         terms: false
+      }
+    },
+    validations: {
+      email: {
+        required,
+        email
+      },
+      age: {
+        required,
+        numeric,
+        minVal: minValue(18)
+      },
+      password: {
+        required,
+        minLen: minLength(8)
+      },
+      confirmPassword: {
+        // sameAs: sameAs('password')
+        sameAs: sameAs( vm => {          
+          return vm.password
+        })
+      },
+      terms: {
+        required: requiredUnless(vm => {
+          return vm.country === 'germany'
+        })
       }
     },
     methods: {
@@ -148,6 +186,15 @@
     outline: none;
     border: 1px solid #521751;
     background-color: #eee;
+  }
+
+  .input.invalid label {
+    color: red;
+  }
+
+  .input.invalid input {
+    border: 1px solid red;
+    background-color: #ffc9aa;
   }
 
   .input select {
